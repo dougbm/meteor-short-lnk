841a1aefa3bb012def1b6a0fec8a5f774d404d99
Douglas <douglas.marquardt@adp.com>
2017-08-17 09:25:54 -0300

    last leasons

--- client/main.js
+++ client/main.js
@@ -2,20 +2,46 @@ import {Meteor} from 'meteor/meteor';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Router, Route, browserHistory} from 'react-router';
+import {Tracker} from 'meteor/tracker';
 
 import Signup from '../imports/ui/Signup';
 import Link from '../imports/ui/Link';
 import NotFound from '../imports/ui/NotFound';
 import Login from '../imports/ui/Login';
 
+const unauthenticatedPages = ['/', '/signup'];
+const authenticatedPages = ['/links'];
+const onEnterPublicPage = () => {
+  if (Meteor.userId()) {
+    browserHistory.replace('/links');
+  }
+};
+const onEnterPrivatePage = () => {
+  if (!Meteor.userId()) {
+    browserHistory.replace('/');
+  }
+};
 const routes = (
   <Router history={browserHistory}>
-    <Route path="/" component={Login}/>
-    <Route path="/signup" component={Signup}/>
-    <Route path="/links" component={Link}/>
+    <Route path="/" component={Login} onEnter={onEnterPublicPage}/>
+    <Route path="/signup" component={Signup} onEnter={onEnterPublicPage}/>
+    <Route path="/links" component={Link} onEnter={onEnterPrivatePage}/>
     <Route path="*" component={NotFound}/>
   </Router>
 );
+
+Tracker.autorun(() => {
+  const isAuthenticated = !!Meteor.userId();
+  const pathName = browserHistory.getCurrentLocation().pathname;
+  const isUnauthenticatedPage = unauthenticatedPages.includes(pathName);
+  const isAuthenticatedPage = authenticatedPages.includes(pathName);
+
+  if (isUnauthenticatedPage && isAuthenticated) {
+    browserHistory.push('/links');
+  } else if (isAuthenticatedPage && !isAuthenticated) {
+    browserHistory.push('/');
+  }
+});
 
 Meteor.startup(() => {
   ReactDOM.render(routes, document.getElementById('app'));
--- server/main.js
+++ server/main.js
@@ -1,5 +1,51 @@
 import { Meteor } from 'meteor/meteor';
+import SimpleSchema from 'simpl-schema';
 
 Meteor.startup(() => {
   // code to run on server at startup
+
+  // const petSchema = new SimpleSchema({
+  //   name: {
+  //     type: String,
+  //     min: 1,
+  //     max: 200,
+  //     optional: true
+  //   },
+  //   age: {
+  //     type: Number,
+  //     min: 0
+  //   },
+  //   contactNumber: {
+  //     type: String,
+  //     optional: true,
+  //     regEx: SimpleSchema.RegEx.Phone
+  //   }
+  // });
+
+  // petSchema.validate({
+  //   age: 21,
+  //   contactNumber: 'aaaa1234'
+  // });
+
+  // const employeeSchema = new SimpleSchema({
+  //   name: {
+  //     type: String,
+  //     min: 1,
+  //     max: 200
+  //   },
+  //   hourlyWage: {
+  //     type: Number,
+  //     min: 0
+  //   },
+  //   email: {
+  //     type: String,
+  //     regEx: SimpleSchema.RegEx.Email
+  //   }
+  // });
+
+  // employeeSchema.validate({
+  //   name: 'Douglas',
+  //   hourlyWage: 100,
+  //   email: 'douglas@test.com'
+  // });
 });
