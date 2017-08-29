ddfba32cdd5d218d3eae0dfc049b4ac7af43316b
Douglas <douglas.marquardt@adp.com>
2017-08-29 11:07:33 -0300

    Updating repo

--- imports/api/links.js
+++ imports/api/links.js
@@ -1,6 +1,7 @@
 import {Meteor} from 'meteor/meteor';
 import {Mongo} from 'meteor/mongo';
 import SimpleSchema from 'simpl-schema';
+import shortid from 'shortid';
 
 export const Links = new Mongo.Collection('links');
 
@@ -26,8 +27,51 @@ Meteor.methods({
     }).validate({url});
 
     Links.insert({
+      _id: shortid.generate(),
       url,
+      userId: this.userId,
+      visible: true,
+      visitedCount: 0,
+      lastVisitedAt: null
+    });
+  },
+  'links.setVisibility'(_id, visible) {
+    if (!this.userId) {
+      throw new Meteor.Error('not-authorized');
+    }
+
+    new SimpleSchema({
+      _id: {
+        type: String,
+        min: 1
+      },
+      visible: {
+        type: Boolean
+      }
+    }).validate({_id, visible});
+
+    Links.update({
+      _id, 
       userId: this.userId
+    }, {
+      $set: {visible}
+    });
+  },
+  'links.trackVisit'(_id) {
+    new SimpleSchema({
+      _id: {
+        type: String,
+        min: 1
+      }
+    }).validate({_id});
+
+    Links.update({_id}, {
+      $set: {
+        lastVisitedAt: new Date().getTime()
+      },
+      $inc: {
+        visitedCount: 1
+      }
     });
   }
 });
--- imports/ui/LinksList.js
+++ imports/ui/LinksList.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import {Meteor} from 'meteor/meteor';
 import {Tracker} from 'meteor/tracker';
+import {Session} from 'meteor/session';
 
 import {Links} from '../api/links';
+import LinksListItem from './LinksListItem';
 
 export default class LinksList extends React.Component {
   constructor(props) {
@@ -14,7 +16,7 @@ export default class LinksList extends React.Component {
   componentDidMount() {
     this.linksTracker = Tracker.autorun(() => {
       Meteor.subscribe('links');
-      const links = Links.find({}).fetch();
+      const links = Links.find({visible: Session.get('showVisible')}).fetch();
       this.setState({links});
     });    
   }
@@ -22,14 +24,21 @@ export default class LinksList extends React.Component {
     this.linksTracker.stop();
   }
   renderLinksListItems () {
+    if (this.state.links.length === 0) {
+      return (
+        <div className="item">
+          <p className="item__status-message">No Links Found</p>
+        </div>
+      );
+    } 
     return this.state.links.map((link) => {
-      return <p key={link._id}>{link.url}</p>;
+      const shortUrl = Meteor.absoluteUrl(link._id);
+      return <LinksListItem key={link._id} shortUrl={shortUrl} {...link}/>
     });
   }
   render() {
     return (
       <div>
-        <p>Links list</p>
         <div>
           {this.renderLinksListItems()}
         </div>
