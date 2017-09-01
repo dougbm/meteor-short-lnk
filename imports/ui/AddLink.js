e93963a679a0c251b5f07868c32669f9ff2a1d3c
Douglas <douglas.marquardt@adp.com>
2017-09-01 14:33:33 -0300

    fix onchange function

--- imports/ui/AddLink.js
+++ imports/ui/AddLink.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import {Meteor} from 'meteor/meteor';
 import Modal from 'react-modal';
+import { Meteor } from 'meteor/meteor';
 
-export default class Link extends React.Component {
+export default class AddLink extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -12,30 +12,29 @@ export default class Link extends React.Component {
     };
   }
   onSubmit(e) {
-    const {url} = this.state;
+    const { url } = this.state;
 
     e.preventDefault();
 
     Meteor.call('links.insert', url, (err, res) => {
       if (!err) {
-        this.handleModalClose;
+        this.handleModalClose();
       } else {
-        this.setState({error: err.reason});
+        this.setState({ error: err.reason });
       }
     });
   }
   onChange(e) {
     this.setState({
-      isOpen: false, 
-      url: '', 
-      error: ''
+      url: e.target.value
     });
   }
   handleModalClose() {
     this.setState({
-      isOpen: false, 
-      url: '', 
-      error: ''});
+      isOpen: false,
+      url: '',
+      error: ''
+    });
   }
   render() {
     return (
@@ -43,7 +42,7 @@ export default class Link extends React.Component {
         <button className="button" onClick={() => this.setState({isOpen: true})}>+ Add Link</button>
         <Modal
           isOpen={this.state.isOpen}
-          contentLabel="Add Link"
+          contentLabel="Add link"
           onAfterOpen={() => this.refs.url.focus()}
           onRequestClose={this.handleModalClose.bind(this)}
           className="boxed-view__box"
@@ -51,17 +50,16 @@ export default class Link extends React.Component {
           <h1>Add Link</h1>
           {this.state.error ? <p>{this.state.error}</p> : undefined}
           <form onSubmit={this.onSubmit.bind(this)} className="boxed-view__form">
-              <input 
+              <input
                 type="text"
-                placeholder="URL" 
+                placeholder="URL"
                 ref="url"
                 value={this.state.url}
-                onChange={this.onChange.bind(this)}
-                />
+                onChange={this.onChange.bind(this)}/>
               <button className="button">Add Link</button>
               <button type="button" className="button button--secondary" onClick={this.handleModalClose.bind(this)}>Cancel</button>
-          </form>                    
-        </Modal> 
+          </form>
+        </Modal>
       </div>
     );
   }
